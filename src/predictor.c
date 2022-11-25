5abb18b4626cefa1bfaeb83e73c5f1e26b877c57
Arpit <gupta.arpit985@gmail.com>
2022-11-24 19:36:51 -0800

    draft: tournament predictor

--- src/predictor.c
+++ src/predictor.c
@@ -7,13 +7,12 @@
 //========================================================//
 #include <stdio.h>
 #include "predictor.h"
+#include <string.h>
+#include <assert.h>
 
-//
-// TODO:Student Information
-//
-const char *studentName = "NAME";
-const char *studentID   = "PID";
-const char *email       = "EMAIL";
+const char *studentName = "Arpit Gupta";
+const char *studentID   = "A59010899";
+const char *email       = "arg005@ucsd.edu";
 
 //------------------------------------//
 //      Predictor Configuration       //
@@ -33,10 +32,219 @@ int verbose;
 //      Predictor Data Structures     //
 //------------------------------------//
 
+// Utilities
+uint8_t update_counter(uint8_t counter, int8_t increment)
+{
+  return (counter + increment) & 0b11;
+}
+
 //
 //TODO: Add your own Branch Predictor data structures here
+struct TournamentPredictor
+{
+    int ghistoryBits;
+    int lhistoryBits;
+    int pcIndexBits;
+
+    // Global history variable.
+    int ghistory;
+
+    // Local history table.
+    // Size: 2^pcIndexBits (each entry is lhistoryBits bits)
+    uint32_t *localHistoryTable;
+
+    // Local predictor.
+    // Size: 2^pcIndexBits (each entry is 2 bits: 00: strongly not taken, 01: weakly not taken, 10: weakly taken, 11: strongly taken)
+    uint8_t *localPrediction;
+
+    // Global predictor.
+    // Size: 2^ghistoryBits (each entry is 2 bits: 00: strongly not taken, 01: weakly not taken, 10: weakly taken, 11: strongly taken)
+    uint8_t *globalPrediction;
+
+    // Choice predictor.
+    // Size: 2^ghistoryBits (each entry is 2 bits: 00: strongly global, 01: weakly global, 10: weakly local, 11: strongly local)
+    uint8_t *choicePrediction;
+};
+
+// Choice constants.
+const uint8_t kTournamentPredictorGlobalChoice = 0;
+const uint8_t kTournamentPredictorLocalChoice = 1;
+
+
+void init_tournament_predictor(struct TournamentPredictor *tournamentPredictor, int ghistoryBits, int lhistoryBits, int pcIndexBits)
+{
+  // Initialize bits in the tournament predictor.
+  tournamentPredictor->ghistoryBits = ghistoryBits;
+  tournamentPredictor->lhistoryBits = lhistoryBits;
+  tournamentPredictor->pcIndexBits = pcIndexBits;
+
+  // Initialize the local history table.
+  uint32_t localHistoryTableSize = (1 << lhistoryBits); // 2^lhistoryBits
+  tournamentPredictor->localHistoryTable = (uint32_t *) malloc(localHistoryTableSize * sizeof(uint32_t));
+  for (int i = 0; i < localHistoryTableSize; i++) { tournamentPredictor->localHistoryTable[i] = 0; }
+  assert(tournamentPredictor->localHistoryTable != NULL);
+
+  // Initialize the local prediction table.
+  uint8_t localPredictionSize = (1 << pcIndexBits); // 2^pcIndexBits
+  tournamentPredictor->localPrediction = (uint8_t *) malloc(localPredictionSize * sizeof(uint8_t));
+  for (int i = 0; i < localPredictionSize; i++) { tournamentPredictor->localPrediction[i] = 1; }
+  assert(tournamentPredictor->localPrediction != NULL);
+
+  // Initialize the global prediction table.
+  uint8_t globalPredictionSize = (1 << ghistoryBits); // 2^ghistoryBits
+  tournamentPredictor->globalPrediction = (uint8_t *) malloc(globalPredictionSize * sizeof(uint8_t));
+  for (int i = 0; i < globalPredictionSize; i++) { tournamentPredictor->globalPrediction[i] = 1; }
+  assert(tournamentPredictor->globalPrediction != NULL);
+
+  // Initialize the choice prediction table.
+  uint8_t choicePredictionSize = (1 << ghistoryBits); // 2^ghistoryBits
+  tournamentPredictor->choicePrediction = (uint8_t *) malloc(choicePredictionSize * sizeof(uint8_t));
+  for (int i = 0; i < choicePredictionSize; i++) { tournamentPredictor->choicePrediction[i] = 0; }
+  assert(tournamentPredictor->choicePrediction != NULL);
+}
+
+void gc_tournament_predictor(struct TournamentPredictor *tournamentPredictor)
+{
+  free(tournamentPredictor->localHistoryTable);
+  free(tournamentPredictor->localPrediction);
+  free(tournamentPredictor->globalPrediction);
+  free(tournamentPredictor->choicePrediction);
+}
+
+uint32_t get_mask(int number_of_ones)
+{
+  uint32_t mask = 0;
+  for (int i = 0; i < number_of_ones; ++i)
+  {
+    mask = (mask << 1) | 1;
+  }
+  return mask;
+}
+
+uint8_t make_prediction_tournament_predictor_local_raw(struct TournamentPredictor *tournamentPredictor, uint32_t pc)
+{
+  // Get the local history at address `pc`.
+  uint32_t pcMask = get_mask(tournamentPredictor->pcIndexBits);
+  uint32_t localHistoryIndex = pc & pcMask; // pc % 2^pcIndexBits
+  // printf("localHistoryIndex: %d\n", localHistoryIndex);
+  // printf("localHistoryTable[0]: %d\n", tournamentPredictor->localHistoryTable[0]);
+  // printf("localHistoryTable[1]: %d\n", tournamentPredictor->localHistoryTable[1]);
+  int localHistory = tournamentPredictor->localHistoryTable[localHistoryIndex];
+  // printf("localhistory value = %d\n", localHistory);
+
+  // Get counter from local prediction.
+  uint8_t localPredictionCounter = tournamentPredictor->localPrediction[localHistory];
+
+  // Return last two bits of the counter.
+  return localPredictionCounter & 0b11;
+}
+
+uint8_t make_prediction_tournament_predictor_local(struct TournamentPredictor *tournamentPredictor, uint32_t pc)
+{
+  uint8_t rawPrediction = make_prediction_tournament_predictor_local_raw(tournamentPredictor, pc);
+
+  // Get the upper bit from the last 2 bits of the counter.
+  return ((rawPrediction >> 1) & 1) == 1 ? TAKEN : NOTTAKEN;
+}
+
+uint8_t make_prediction_tournament_predictor_global_raw(struct TournamentPredictor *tournamentPredictor)
+{
+  // Get the global history.
+  int globalHistory = tournamentPredictor->ghistory;
+
+  // Get counter from global prediction.
+  uint8_t globalPredictionCounter = tournamentPredictor->globalPrediction[globalHistory];
+
+  // Return last two bits of the counter.
+  return globalPredictionCounter & 0b11;
+}
+
+uint8_t make_prediction_tournament_predictor_global(struct TournamentPredictor *tournamentPredictor)
+{
+  uint8_t rawPrediction = make_prediction_tournament_predictor_global_raw(tournamentPredictor);
+
+  // Get the upper bit from the last 2 bits of the counter.
+  return ((rawPrediction >> 1) & 1) == 1 ? TAKEN : NOTTAKEN;
+}
+
+uint8_t get_choice_tournament_predictor(struct TournamentPredictor *tournamentPredictor)
+{
+  // Get the global history.
+  int globalHistory = tournamentPredictor->ghistory;
+
+  // Get counter from choice prediction.
+  uint8_t choicePredictionCounter = tournamentPredictor->choicePrediction[globalHistory];
+
+  // Get the upper bit from the last 2 bits of the counter.
+  uint8_t choice = (choicePredictionCounter >> 1) & 1;
+
+  return choice ? kTournamentPredictorLocalChoice : kTournamentPredictorGlobalChoice;
+}
+
+uint8_t make_prediction_tournament_predictor(struct TournamentPredictor *tournamentPredictor, uint32_t pc)
+{
+  uint8_t prediction = 0;
+  uint8_t choice = get_choice_tournament_predictor(tournamentPredictor);
+
+  switch(choice)
+  {
+    case kTournamentPredictorGlobalChoice:
+      prediction = make_prediction_tournament_predictor_global(tournamentPredictor);
+      break;
+    case kTournamentPredictorLocalChoice:
+      prediction = make_prediction_tournament_predictor_local(tournamentPredictor, pc);
+      break;
+    default:
+      break;
+  }
+  return prediction;
+}
+
+void train_tournament_predictor(struct TournamentPredictor *tournamentPredictor, uint32_t pc, uint8_t outcome) {
+  // Update choice predictor.
+  // printf("Starting train predictor torunament\n");
+  uint8_t local_prediction_raw = make_prediction_tournament_predictor_local_raw(tournamentPredictor, pc);
+  // printf("local_prediction_raw: %d\n", local_prediction_raw);
+  uint8_t local_prediction = (local_prediction_raw >> 1) & 1;
+  // printf("local_prediction: %d\n", local_prediction);
+
+  uint8_t global_prediction_raw = make_prediction_tournament_predictor_global_raw(tournamentPredictor);
+  uint8_t global_prediction = (global_prediction_raw >> 1) & 1;
+
+  // printf("local_prediction: %d, global_prediction: %d\n", local_prediction, global_prediction);
+  if (local_prediction != global_prediction)
+  {
+    int8_t increment = (local_prediction == outcome) ? 1 : -1;
+    uint8_t* choicePrediction = &tournamentPredictor->choicePrediction[tournamentPredictor->ghistory];
+    *choicePrediction = update_counter(*choicePrediction, increment);
+  }
+
+  // Update local predictor.
+  // printf("Updating local predictor\n");
+
+  // Get the local history at address `pc`.
+  uint32_t pcMask = get_mask(tournamentPredictor->pcIndexBits);
+  uint32_t localHistoryIndex = pc & pcMask; // pc % 2^pcIndexBits
+  uint32_t *localHistory = &tournamentPredictor->localHistoryTable[localHistoryIndex];
+  // Update local prediction.
+  // Get counter from local prediction.
+  uint8_t *localPredictionCounter = &tournamentPredictor->localPrediction[*localHistory];
+  // Update counter.
+  *localPredictionCounter = update_counter(*localPredictionCounter, outcome);
+  // Update local history.
+  *localHistory = ((*localHistory << 1) | outcome) & pcMask;
+
+  // Update global predictor.
+  // Get the global history.
+  int globalHistory = tournamentPredictor->ghistory;
+  // Get counter from global prediction.
+  uint8_t* globalPredictionCounter = &tournamentPredictor->globalPrediction[globalHistory];
+  // Update counter.
+  *globalPredictionCounter = update_counter(*globalPredictionCounter, outcome);
+}
 //
 
+struct TournamentPredictor tournamentPredictor;
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -47,9 +255,19 @@ int verbose;
 void
 init_predictor()
 {
-  //
-  //TODO: Initialize Branch Predictor Data Structures
-  //
+  switch(bpType) {
+    case STATIC:
+      break;
+    case GSHARE:
+      break;
+    case TOURNAMENT:
+      init_tournament_predictor(&tournamentPredictor, ghistoryBits, lhistoryBits, pcIndexBits);
+      break;
+    case CUSTOM:
+      break;
+    default:
+      break;
+  }
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -69,6 +287,7 @@ make_prediction(uint32_t pc)
       return TAKEN;
     case GSHARE:
     case TOURNAMENT:
+      return make_prediction_tournament_predictor(&tournamentPredictor, pc);
     case CUSTOM:
     default:
       break;
@@ -88,4 +307,18 @@ train_predictor(uint32_t pc, uint8_t outcome)
   //
   //TODO: Implement Predictor training
   //
+  switch (bpType)
+  {
+    case STATIC:
+      break;
+    case GSHARE:
+      break;
+    case TOURNAMENT:
+      train_tournament_predictor(&tournamentPredictor, pc, outcome);
+      break;
+    case CUSTOM:
+      break;
+    default:
+      break;
+  }
 }
